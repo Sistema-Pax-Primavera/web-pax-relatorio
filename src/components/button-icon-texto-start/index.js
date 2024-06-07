c7f634323376d125485f2e219cc0b742a82f8a94
Diogo Perez <perezdiogo1@gmail.com>
2024-06-07 15:05:00 -0400

    style:add estilos do ButtonIconTextoStart

--- src/components/button-icon-texto-start/index.js
+++ src/components/button-icon-texto-start/index.js
@@ -2,14 +2,21 @@ import React from "react";
 import "./icone-button.css";
 import Button from "@mui/material/Button";
 
-const ButtonIconTextoStart = ({ icon, title, funcao, corFundoBotao, corTextoBotao, fontSizeBotao, fontWeightBotao,alinhamentoBotao, width, marginTop }) => {
+const ButtonIconTextoStart = ({ icon, title, funcao, corFundoBotao, corTextoBotao, fontSizeBotao, fontWeightBotao, alinhamentoBotao, width, marginTop }) => {
   const estilos = {
     width: width,
-    marginTop:marginTop
+    marginTop: marginTop
   };
   return (
     <div className="icones-buttons-start" style={estilos}>
-      <Button onClick={funcao} style={{ backgroundColor: corFundoBotao, color: corTextoBotao, fontSize: fontSizeBotao, fontWeight:fontWeightBotao, alignItems: alinhamentoBotao }}>
+      <Button onClick={funcao}
+        tyle={{
+          backgroundColor: corFundoBotao,
+          color: corTextoBotao,
+          fontSize: fontSizeBotao,
+          fontWeight: fontWeightBotao,
+          alignItems: alinhamentoBotao
+        }}>
         {icon}
         {title}
       </Button>
